3d4da62cfe852c0be2b5d4a8be482a46dd01c51c
vypht <phamhoanthienvyk12@siu.edu.vn>
2022-08-09 09:24:59 +0700

    Project RESTful Service

---
+++ src/test/java/com/app/restful_service/ResTfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.restful_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
